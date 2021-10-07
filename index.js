b972664c7bd3b0d4d44b2e63c53d6d8fc96a0c99
vagnerdavid <vagnerdavid00@gmail.com>
2021-10-07 13:38:45 +0200

    dolgok

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const port = 4444;
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.write(`A lérés forrása : $(req.url}`)
-    res.end("     vége");
+    
+    console.log(req.url);
+    console.log(req.method);
+    
+    
+    //router
+    switch (true) {
+        case req.url === '/' && req.method === 'GET':
+            fs.readFile('./views/index.html', (err.file)=>{
+                res.setHeader('Content-Type', 'text/html');
+                res.end(file);
+            });
+            break;
+        default:
+            res.end('404 Not Found');
+            //Majd ezt is küldöm állományba
+    }
 })
 
 server.listen(port);
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
